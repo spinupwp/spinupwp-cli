3abd0cfafdf80edb9d2bdae9df9f208b58b9ea92
realdanielmlozano <daniel@danielmlozano.com>
2021-10-04 14:09:53 -0500

    Bug fix

--- app/Helpers/Configuration.php
+++ app/Helpers/Configuration.php
@@ -60,7 +60,7 @@ class Configuration
             case "WINNT":
                 $username = getenv('username');
                 $path = config('app.windows_path');
-                // no break
+                break;
             case "Darwin":
                 $username = trim(shell_exec("whoami"));
                 $path = config('app.macos_path');
