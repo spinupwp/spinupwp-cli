002d4a77c0ff548ec68ec8691c7bee317cf00a7e
realdanielmlozano <daniel@danielmlozano.com>
2021-10-13 16:26:51 -0500

    Fixes

--- app/Commands/Configure.php
+++ app/Commands/Configure.php
@@ -7,15 +7,15 @@ use LaravelZero\Framework\Commands\Command;
 
 class Configure extends Command
 {
-    protected $signature = 'configure {--team=}';
+    protected $signature = 'configure {--profile=}';
 
     protected $description = 'Configure the SpinupWP\'s API token';
 
     public function handle()
     {
-        $team = $this->option('team') ?? 'default';
+        $team = $this->option('profile') ?? 'default';
 
-        if (!empty(Configuration::getCredentials($team))) {
+        if (!empty(Configuration::get('api_token', $team))) {
             $this->alert("The {$team} team is already configured");
             $response = $this->ask('Do you want to reconfigure and overwrite existing credentials? (y/n)', 'y');
             while (!in_array($response, ['y', 'n'])) {
@@ -39,7 +39,7 @@ class Configure extends Command
             $defaultFormat = $this->ask('Which format would you prefer for data output? (json/table)', null);
         }
 
-        Configuration::saveCredentials($apiKey, $defaultFormat, $team);
+        Configuration::saveConfig($apiKey, $defaultFormat, $team);
         $this->info("SpinupWP CLI configured successfully");
         return 0;
     }
