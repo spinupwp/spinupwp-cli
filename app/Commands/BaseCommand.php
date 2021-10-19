63e357f1e2327e327892a1c8be059bfc25bdc39a
realdanielmlozano <daniel@danielmlozano.com>
2021-10-19 17:29:25 -0500

    Formats wip

--- app/Commands/BaseCommand.php
+++ app/Commands/BaseCommand.php
@@ -3,15 +3,20 @@
 namespace App\Commands;
 
 use App\Helpers\Configuration;
+use DeliciousBrains\SpinupWp\Resources\ResourceCollection;
 use DeliciousBrains\SpinupWp\SpinupWp;
 use GuzzleHttp\Client as HttpClient;
+use Illuminate\Support\Collection;
 use LaravelZero\Framework\Commands\Command;
+use LucidFrame\Console\ConsoleTable;
 
 abstract class BaseCommand extends Command
 {
     protected Configuration $config;
 
     protected SpinupWp $spinupwp;
+
+    protected array $table;
 
     public function __construct(Configuration $configuration)
     {
@@ -44,8 +49,6 @@ abstract class BaseCommand extends Command
         return 0;
     }
 
-    abstract protected function action();
-
     protected function apiToken(): string
     {
         return $this->config->get('api_token');
@@ -54,11 +57,11 @@ abstract class BaseCommand extends Command
     protected function format($resource)
     {
         $format = $this->config->get('format');
-        if (!is_string($this->option('format'))) {
+        if (is_string($this->option('format'))) {
             $format = $this->option('format');
         }
         if ($format !== 'json') {
-            //
+            return $this->toTable($resource);
         }
         return $this->toJson($resource);
     }
@@ -67,4 +70,43 @@ abstract class BaseCommand extends Command
     {
         return json_encode($resource->toArray(), JSON_PRETTY_PRINT);
     }
+
+    protected function toTable($resource)
+    {
+        $table        = new ConsoleTable();
+        $tableHeaders = [];
+        $tableRows    = [];
+
+        if ($resource instanceof Collection) {
+            $firstElement = $resource->first();
+
+            if (!is_array($firstElement)) {
+                $firstElement = $firstElement->toArray();
+            }
+
+            $tableHeaders = array_keys($firstElement);
+
+            foreach ($tableHeaders as $header) {
+                $table->addHeader($header);
+            }
+
+            $resource->each(function ($item) use ($table) {
+                $table->addRow();
+                if (!is_array($item)) {
+                    $item->toArray();
+                }
+                $row = array_values($item);
+                foreach ($row as $value) {
+                    if (!is_string($value)) {
+                        $value = '';
+                    }
+                    $table->addColumn($value);
+                }
+            });
+        }
+
+        return $table->display();
+    }
+
+    abstract protected function action();
 }
