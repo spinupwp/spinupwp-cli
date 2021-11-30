7dbf03f67781d0234e96e574d7b47f9d0c8cda19
realdanielmlozano <daniel@danielmlozano.com>
2021-11-30 14:56:16 -0600

    Save fields filter in the configuration

--- app/Commands/Concerns/SpecifyFields.php
+++ app/Commands/Concerns/SpecifyFields.php
@@ -6,7 +6,7 @@ trait SpecifyFields
 {
     protected array $fieldsMap = [];
 
-    protected function specifyFields($resource): array
+    protected function specifyFields($resource, array $fieldsFilter = []): array
     {
         if (empty($this->fieldsMap)) {
             return $resource->toArray();
@@ -14,8 +14,17 @@ trait SpecifyFields
 
         $fields = [];
 
+        $commandFields = $this->config->getCommandConfiguration($this->command, $this->profile())['fields'] ?? null;
+
+        if ($commandFields) {
+            $fieldsFilter = explode(',', str_replace(' ', '', $commandFields));
+        }
+
         if ($this->option('fields')) {
-            $fieldsFilter    = explode(',', str_replace(' ', '', $this->option('fields')));
+            $fieldsFilter = explode(',', str_replace(' ', '', $this->option('fields')));
+        }
+
+        if (!empty($fieldsFilter)) {
             $this->fieldsMap = array_filter($this->fieldsMap, function ($field) use ($fieldsFilter) {
                 if (!is_array($field)) {
                     return in_array($field, $fieldsFilter);
@@ -47,5 +56,19 @@ trait SpecifyFields
         }
 
         return $fields;
+    }
+
+    protected function saveFieldsFilter(): void
+    {
+        $commandOptions    = $this->config->getCommandConfiguration($this->command, $this->profile());
+        $saveConfiguration = false;
+
+        if (empty($commandOptions)) {
+            $saveConfiguration = $this->confirm('Do you want to save the specified fields as default for this command', true);
+        }
+
+        if ($saveConfiguration) {
+            $this->config->setCommandConfiguration($this->command, 'fields', $this->option('fields'), $this->profile());
+        }
     }
 }
