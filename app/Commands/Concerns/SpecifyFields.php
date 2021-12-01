cfbbd2dabdf800983b3af378fc7ac1fba473d71e
realdanielmlozano <daniel@danielmlozano.com>
2021-12-01 12:19:54 -0600

    Save empty fields

--- app/Commands/Concerns/SpecifyFields.php
+++ app/Commands/Concerns/SpecifyFields.php
@@ -69,6 +69,9 @@ trait SpecifyFields
 
         if ($saveConfiguration) {
             $this->config->setCommandConfiguration($this->command, 'fields', $this->option('fields'), $this->profile());
+            return;
         }
+
+        $this->config->setCommandConfiguration($this->command, 'fields', '', $this->profile());
     }
 }
