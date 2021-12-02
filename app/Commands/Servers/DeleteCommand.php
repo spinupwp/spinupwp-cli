657c77497c2e4df6024f5f2ed3922386429e0229
realdanielmlozano <daniel@danielmlozano.com>
2021-12-02 15:01:36 -0600

    Delete server command WIP

---
+++ app/Commands/Servers/DeleteCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Commands\Servers;
+
+use App\Commands\BaseCommand;
+
+class DeleteCommand extends BaseCommand
+{
+    protected $signature = 'servers:delete {id?} {--force} {--profile=}';
+
+    protected $description = 'Delete a server';
+
+    protected $simpleOutput = true;
+
+    public function action()
+    {
+        $serverId = $this->argument('id');
+
+        $delete = $this->option('force') || $this->confirm('Are you sure you want to delete this server?');
+
+        if ($delete) {
+            $response = $this->spinupwp->servers->delete($serverId);
+            $this->info("Server deletion in progress. Event ID: {$response}");
+        }
+    }
+}
