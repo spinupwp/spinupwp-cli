54ca43067786618b4d88e8c6e5b6db52bebbadea
realdanielmlozano <daniel@danielmlozano.com>
2021-12-02 15:01:30 -0600

    Test

---
+++ tests/Feature/Commands/ServersDeleteCommandTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use GuzzleHttp\Psr7\Response;
+
+beforeEach(function () {
+    setTestConfigFile();
+    $this->clientMock->shouldReceive('request')->with('DELETE', 'servers/1', [
+        'form_params' => ['delete_server_on_provider' => false],
+    ])->andReturn(
+        new Response(200, [], json_encode(['event_id' => '100']))
+    );
+});
+
+afterEach(function () {
+    deleteTestConfigFile();
+});
+
+test('delete a server with force option', function () {
+    $this->artisan('servers:delete 1 --force')->expectsOutput('Server deletion in progress. Event ID: 100');
+});
+
+test('delete a server without force option', function () {
+    $this->artisan('servers:delete 1')
+        ->expectsConfirmation('Are you sure you want to delete this server?', 'yes')
+        ->expectsOutput('Server deletion in progress. Event ID: 100');
+});
+
+test('delete a server without force option and cancel deletion', function () {
+    $this->artisan('servers:delete 1')
+        ->expectsConfirmation('Are you sure you want to delete this server?', 'no')
+        ->doesntExpectOutput('Server deletion in progress. Event ID: 100');
+});
