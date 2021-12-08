d20ef40d277e005e6aef4c1d04248b1d8c7f3241
Daniel Mendoza <daniel@inweb.mx>
2021-12-08 13:46:51 -0500

    Delete on provider

--- app/Commands/Servers/DeleteCommand.php
+++ app/Commands/Servers/DeleteCommand.php
@@ -6,21 +6,27 @@ use App\Commands\BaseCommand;
 
 class DeleteCommand extends BaseCommand
 {
-    protected $signature = 'servers:delete {id?} {--force} {--profile=}';
+    protected $signature = 'servers:delete {id?} {--force} {--profile=} {--delete-on-provider}';
 
     protected $description = 'Delete a server';
 
     protected $simpleOutput = true;
 
-    public function action()
+    public function action(): int
     {
         $serverId = $this->argument('id');
+
+        if (empty($serverId)) {
+            $serverId = $this->askToSelectServer('Which server would you like to start an SSH session for');
+        }
 
         $delete = $this->option('force') || $this->confirm('Are you sure you want to delete this server?');
 
         if ($delete) {
-            $response = $this->spinupwp->servers->delete($serverId);
+            $response = $this->spinupwp->servers->delete($serverId, (bool) $this->option('delete-on-provider'));
             $this->info("Server deletion in progress. Event ID: {$response}");
         }
+
+        return self::SUCCESS;
     }
 }
