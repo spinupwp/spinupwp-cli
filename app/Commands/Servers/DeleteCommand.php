efc8cf7e795a0232d7c10e350e4b3324b53ff458
realdanielmlozano <daniel@danielmlozano.com>
2021-12-13 14:19:50 -0600

    Options

--- app/Commands/Servers/DeleteCommand.php
+++ app/Commands/Servers/DeleteCommand.php
@@ -6,7 +6,11 @@ use App\Commands\BaseCommand;
 
 class DeleteCommand extends BaseCommand
 {
-    protected $signature = 'servers:delete {id?} {--force} {--profile=}';
+    protected $signature = 'servers:delete
+                            {server_id? : The server to delete}
+                            {--d|delete-on-provider : Delete the server from the server provider (DigitalOcean, etc.)}
+                            {--f|force : Delete the server without prompting for confirmation}
+                            {--profile=}';
 
     protected $description = 'Delete a server';
 
@@ -14,7 +18,7 @@ class DeleteCommand extends BaseCommand
 
     public function action(): int
     {
-        $serverId = $this->argument('id');
+        $serverId = $this->argument('server_id');
 
         $delete = $this->option('force') || $this->confirm('Are you sure you want to delete this server?');
 
