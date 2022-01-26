4880f5f132a031f59f3229ee12c303cc2b92ed1f
ohryan <ohryan@users.noreply.github.com>
2022-01-26 22:36:29 +0000

    Apply php-cs-fixer changes

--- app/Helpers/OptionsHelper.php
+++ app/Helpers/OptionsHelper.php
@@ -5,9 +5,9 @@ namespace App\Helpers;
 class OptionsHelper
 {
     // SpinupWP CLI only supports a subset of installation methods available via the REST API
-    const INSTALLATION_METHODS = ['wp', 'blank'];
+    public const INSTALLATION_METHODS = ['wp', 'blank'];
 
-    const PHP_VERSIONS = [
+    public const PHP_VERSIONS = [
         '80' => '8.0',
         '74' => '7.4',
     ];
