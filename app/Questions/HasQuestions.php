19d7f4110a1108fa56a769a88a163bfd4faa596d
realdanielmlozano <daniel@danielmlozano.com>
2022-04-22 13:45:50 -0500

    Add key property and skippable capability

--- app/Questions/HasQuestions.php
+++ app/Questions/HasQuestions.php
@@ -14,12 +14,21 @@ trait HasQuestions
     {
         $answers = [];
         foreach ($this->questions() as $question) {
+            if ($question->skip) {
+                continue;
+            }
+
             $question->nonInteractive($nonInteractive);
             $answers += [
-                $question->flag => $question->resolveAnswer($this),
+                $this->getAnswerKey($question) => $question->resolveAnswer($this),
             ];
         }
 
         return $answers;
+    }
+
+    private function getAnswerKey(Question $question): string
+    {
+        return !empty($question->flag) ? $question->flag : $question->key;
     }
 }
