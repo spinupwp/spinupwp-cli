6c4793a29e7398028ffbe6a61a11194265af85a2
realdanielmlozano <daniel@danielmlozano.com>
2022-04-22 15:12:03 -0500

    Testing

---
+++ tests/Feature/Commands/DigitalOceanConnectCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+beforeEach(function () {
+    setTestConfigFile();
+    setTestMysqlPwdFile();
+});
+
+afterEach(function () {
+    deleteTestConfigFile();
+    deleteTestMysqlPwdFile();
+});
+
+test('digitalocean connect command', function () {
+    $this->artisan('digitalocean:connect')
+        ->expectsOutput('Welcome to the SpinupWP 1-click app. This wizard will allow you to connect your brand new server to your SpinupWP account')
+        ->expectsQuestion('Do you want to continue?', 'yes')
+        ->expectsOutput('Database Root Password')
+        ->expectsQuestion('Do you want to generate a random password?', false)
+        ->expectsQuestion('Enter a new MySQL root password', '123')
+        ->expectsOutput('Your MySQL root password: 123')
+        ->expectsOutput("Make sure to note this password down somewhere (or save it in a Password Manager) as we won't show it again.")
+        ->expectsQuestion('Press Enter to continue', true)
+        ->expectsOutput('Changing MySQL root password')
+        ->expectsOutput('MySQL root password changed.');
+});
